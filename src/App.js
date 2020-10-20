890ecfe0c4c3745d2eff6cfae44b6cf27c8cfbfa
betty <betty@marketdesigners.com>
2020-10-20 23:34:13 +0900

    login page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { HashRouter as Router, Route, Switch } from 'react-router-dom'
 import AppHeader from './components/common/AppHeader'
 import LoginPage from './pages/LoginPage'
 import SignupPage from './pages/SignupPage'
+import Main from './pages/Main'
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
         <Switch>
           <Route exact path={'/login'} component={LoginPage}></Route>
           <Route exact path={'/signup'} component={SignupPage}></Route>
+          <Route exact path="/main" component={Main} />
         </Switch>
       </Router>
     )
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,22 +1,54 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
+import instance from '../api/Api'
 
 class LoginPage extends Component {
-  render() {
-    return (
-      <div>
+	constructor(props){
+		super(props)
+		this.state = {
+			username: '',
+			password: '',
+		}
+	}
+
+	onChangeForm = (e) => {
+		const target = e.target
+		const name = target.name
+		const value = target.value
+
+		this.setState({
+			[name]: value
+		})
+	}
+
+	onSubmitForm = (e) => {
+		e.preventDefault();
+
+		instance.post('/login', {username: this.state.username , password: this.state.password})
+			.then((response) => {
+				console.log(response.data.user)
+				const nickname = response.data.user.nickname
+				alert(`${nickname}님 반갑습니당. 메인으로 이동쓰~!`)
+				this.props.history.push('/main')
+			})
+			.catch(error => console.log(error.response))
+		
+	}
+	render() {
+		return (
+			<div>
         <h1 className="page-header">로그인</h1>
         <div className="contents">
           <div className="form-wrapper form-wrapper-sm">
-            <form action="" className="form">
+            <form action="" className="form" onSubmit={this.onSubmitForm} >
               <div>
                 <label htmlFor="username">id:</label>
-                <input id="username" type="text" />
+                <input id="username" type="text" name="username" onChange={this.onChangeForm}/>
               </div>
               <div>
                 <label htmlFor="password">pw: </label>
-                <input id="password" type="password" />
+                <input id="password" type="password" name="password" onChange={this.onChangeForm} />
               </div>
-              <button type="submit" className="btn">
+              <button type="submit" className="btn" >
                 로그인
               </button>
             </form>
@@ -24,8 +56,8 @@ class LoginPage extends Component {
           </div>
         </div>
       </div>
-    );
-  }
+		)
+	}
 }
 
-export default LoginPage;
+export default LoginPage
