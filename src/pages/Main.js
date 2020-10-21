b15f047d504396820ca8432e397d397f5cf6ec55
betty <betty@marketdesigners.com>
2020-10-21 23:33:19 +0900

    게시글 리스트

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,13 +1,46 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+import Axios from "axios";
+import instance from "../api/Api";
 
-export class Main extends Component {
-	render() {
-		return (
-			<div>
-				main
-			</div>
-		)
+// 메인페이지
+class MainPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+	componentDidMount(){
+		instance.get('/posts')
+			.then(response => console.log(response))
+			.catch()
 	}
+
+  render() {
+    return (
+      <div>
+        <h1 className="page-header">학습노트 리스트</h1>
+        <div className="main list-container contents">
+          <ul>
+            <li>
+              <div className="post-title">타이틀</div>
+              <div className="post-contents">제목</div>
+              <div className="post-time">
+                2020-09-18
+                <i className="icon ion-md-create"></i>
+                <i className="icon ion-md-trash"></i>
+              </div>
+            </li>
+          </ul>
+        </div>
+        {/* <div className="empty-content">
+          <h3>등록된 학습노트가 없습니다.</h3>
+        </div> */}
+        <NavLink to={"/write"} className="create-button">
+          <i className="ion-md-add"></i>
+        </NavLink>
+      </div>
+    );
+  }
 }
 
-export default Main
+export default MainPage;
