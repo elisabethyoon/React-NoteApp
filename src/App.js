af28be2d15a5e0c62985be590f45bfd3806b23db
betty <betty@marketdesigners.com>
2020-10-21 23:32:48 +0900

    게시글 쓰기 markup

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AppHeader from './components/common/AppHeader'
 import LoginPage from './pages/LoginPage'
 import SignupPage from './pages/SignupPage'
 import Main from './pages/Main'
+import ListWrite from './pages/ListWrite'
 
 class App extends Component {
   render() {
@@ -14,6 +15,7 @@ class App extends Component {
           <Route exact path={'/login'} component={LoginPage}></Route>
           <Route exact path={'/signup'} component={SignupPage}></Route>
           <Route exact path="/main" component={Main} />
+          <Route exact path="/write" component={ListWrite}></Route>
         </Switch>
       </Router>
     )
--- src/components/common/AppHeader.js
+++ src/components/common/AppHeader.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom'
 
 class AppHeader extends Component {
+	constructor(props) {
+		super(props)
+		
+	}
 	render() {
 		return (
 			<header>
