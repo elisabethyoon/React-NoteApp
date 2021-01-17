76e0f6a5edec3e4519eba92da00d1ae31bacfe3a
elisabeth <careerwoman87@gmail.com>
2021-01-17 18:40:55 +0900

    login / signup / logout / history / api

--- src/App.js
+++ src/App.js
@@ -4,15 +4,42 @@ import { Route, Switch } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import SingupPage from "./pages/SingupPage";
 import ErrorPage from "./pages/ErrorPage";
+import MainPage from "./pages/MainPage";
+import history from "./utils/history";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      token: ""
+    };
+  }
+  login = (token) => {
+    console.log(token, "받아땅");
+    this.setState({
+      token: token
+    });
+  };
+
+  logout = () => {
+    console.log("하하하");
+    this.setState({
+      token: ""
+    });
+    history.push("/login");
+  };
   render() {
     return (
       <div>
-        <AppHeader />
+        <AppHeader token={this.state.token} logout={this.logout} />
         <Switch>
-          <Route exact path={["/", "/login"]} component={LoginPage}></Route>
+          <Route
+            exact
+            path={["/", "/login"]}
+            render={(props) => <LoginPage {...props} login={this.login} />}
+          ></Route>
           <Route exact path="/signup" component={SingupPage}></Route>
+          <Route exact path="/main" component={MainPage}></Route>
           <Route exact path="*" component={ErrorPage}></Route>
         </Switch>
       </div>
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { AST_Await } from "terser";
+import Api from "../utils/Api";
+import history from "../utils/history";
 
 export class Login extends Component {
   constructor(props) {
@@ -17,19 +18,40 @@ export class Login extends Component {
       [name]: value
     });
   };
+
+  // 로그인 버튼 클릭
+  login = () => {
+    const { username, password } = this.state;
+    const apiparams = {
+      username: username,
+      password: password
+    };
+    Api.post("login", apiparams)
+      .then((response) => {
+        const token = response.data.token;
+        this.props.login(token);
+        const username = response.data.user.username;
+        alert(`${username}님 반가워용! 메인으로 이동할게유`);
+        history.push("/main");
+      })
+      .catch((error) => console.log(error));
+    // axios를 이용하여 로그인 정보를 전달하고 응답값을 받아온다.
+  };
   render() {
+    const { username, password } = this.state;
     return (
       <div>
         <h1 className="page-header">로그인</h1>
         <div className="contents">
           <div className="form-wrapper form-wrapper-sm">
-            <form action="" className="form">
+            <div className="form">
               <div>
                 <label htmlFor="username">id:</label>
                 <input
                   id="username"
                   type="text"
                   name="username"
+                  value={username}
                   onChange={this.onChangeValue}
                 />
               </div>
@@ -39,13 +61,14 @@ export class Login extends Component {
                   id="password"
                   type="password"
                   name="password"
+                  value={password}
                   onChange={this.onChangeValue}
                 />
               </div>
-              <button type="submit" className="btn">
+              <button type="button" className="btn" onClick={this.login}>
                 로그인
               </button>
-            </form>
+            </div>
             <p className="log"></p>
           </div>
         </div>
